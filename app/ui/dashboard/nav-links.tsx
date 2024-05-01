38411b398167c10895f396c010a59512aa56c614
mefazdev <fazdev.com@gmail.com>
2024-05-01 10:31:57 +0530

    initial

--- app/ui/dashboard/nav-links.tsx
+++ app/ui/dashboard/nav-links.tsx
@@ -1,3 +1,4 @@
+'use client'
 import {
   UserGroupIcon,
   HomeIcon,
@@ -16,7 +17,10 @@ const links = [
   { name: 'Customers', href: '/dashboard/customers', icon: UserGroupIcon },
 ];
 
+import clsx from 'clsx';
+import { usePathname } from 'next/navigation';
 export default function NavLinks() {
+  const pathname = usePathname()
   return (
     <>
       {links.map((link) => {
@@ -25,7 +29,13 @@ export default function NavLinks() {
           <a
             key={link.name}
             href={link.href}
-            className="flex h-[48px] grow items-center justify-center gap-2 rounded-md bg-gray-50 p-3 text-sm font-medium hover:bg-sky-100 hover:text-blue-600 md:flex-none md:justify-start md:p-2 md:px-3"
+            className={clsx(
+              'flex h-[48px] grow items-center justify-center gap-2 rounded-md bg-gray-50 p-3 text-sm font-medium hover:bg-sky-100 hover:text-blue-600 md:flex-none md:justify-start md:p-2 md:px-3',
+              {'bg-sky-100 text-blue-600' : pathname== link.href}
+
+            )}
+            
+            
           >
             <LinkIcon className="w-6" />
             <p className="hidden md:block">{link.name}</p>
